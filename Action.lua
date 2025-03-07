6af3d6dcfc94eccdfdca1b36ecc8e94cc3bd50f5
MisterCrab <51509374+MisterCrab@users.noreply.github.com>
2025-03-08 01:24:58 +0300

    Fixed :14666: attempt to index field '?' nil value

--- Action.lua
+++ Action.lua
@@ -1,5 +1,5 @@
 --- 
-local DateTime 														= "26.12.2024"
+local DateTime 														= "08.03.2025"
 ---
 local pcall, ipairs, pairs, type, assert, error, setfenv, getmetatable, setmetatable, loadstring, next, unpack, select, _G, coroutine, table, math, string = 
 	  pcall, ipairs, pairs, type, assert, error, setfenv, getmetatable, setmetatable, loadstring, next, unpack, select, _G, coroutine, table, math, string
@@ -14726,7 +14726,7 @@ function Action.ToggleMainUI()
 			--	-- Druid in Classic hasn't ressurect
 			--	SelectResurrects:Disable()
 			--	SelectResurrects:SetChecked(false, true) -- only internal 
-			--	specDB[db].SelectResurrects = false 
+			--	specDB.SelectResurrects = false 
 			--end 
 		end -- isHealer END 
 
