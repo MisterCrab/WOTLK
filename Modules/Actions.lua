19f04d7b79f6b522173f2e088a39b8698053a197
MisterCrab <51509374+MisterCrab@users.noreply.github.com>
2025-06-24 20:20:12 +0300

    MetaEngine: Fixed lag spikes

--- Modules/Actions.lua
+++ Modules/Actions.lua
@@ -689,9 +689,9 @@ end
 
 -- "LEARNED_SPELL_IN_TAB" > "TRAINER_UPDATE" > "SKILL_LINES_CHANGED"
 -- "LEARNED_SPELL_IN_TAB" new added
--- "SKILL_LINES_CHANGED" new added / existing level (rank) update
+-- "SKILL_LINES_CHANGED" new added / existing level (rank) update, replaced to "TRAINER_UPDATE" because of lag spikes on MetaEngine
 Listener:Add("ACTION_EVENT_SPELL_RANKS", "LEARNED_SPELL_IN_TAB", 		A.UpdateSpellBook)
-Listener:Add("ACTION_EVENT_SPELL_RANKS", "SKILL_LINES_CHANGED", 		A.UpdateSpellBook) 
+Listener:Add("ACTION_EVENT_SPELL_RANKS", "TRAINER_UPDATE", 				A.UpdateSpellBook) 
 TMW:RegisterCallback("TMW_ACTION_TALENT_MAP_UPDATED", function()
 	A.UpdateSpellBook()
 end)
